b3105f38a07213c369164b24cba97538d7f6bd6a
wesllycode <wesllycode07@gmail.com>
2023-02-20 11:40:21 -0300

    feat: finalizado aula 3

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/',[HomeController::class,'index']);
+Route::get('/',[HomeController::class,'index'])->name('home');
 
 Route::get('/product/{product}',[ProdutoController::class, 'show'])->name('product');
 
